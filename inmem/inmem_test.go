caa60040647bf918c038eab6e87f8fd333d3a264
Solomon Hykes <solomon@docker.com>
2014-05-09 21:12:27 -0700

    inmem: use pkg/testutils
    Signed-off-by: Solomon Hykes <solomon@docker.com>

--- inmem/inmem_test.go
+++ inmem/inmem_test.go
@@ -1,27 +1,37 @@
 package inmem
 
 import (
+	"github.com/dotcloud/docker/pkg/testutils"
 	"testing"
-	"time"
 )
 
 func TestSimpleSend(t *testing.T) {
 	r, w := Pipe()
-	onTimeout := time.After(100 * time.Millisecond)
-	onRcv := make(chan bool)
-	go func() {
-		msg, in, out, err := r.Receive(0)
+	testutils.Timeout(t, func() {
+		go func() {
+			msg, in, out, err := r.Receive(0)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if msg.Data != "hello world" {
+				t.Fatalf("%#v", *msg)
+			}
+			if msg.Name != "print" {
+				t.Fatalf("%#v", *msg)
+			}
+			if len(msg.Args) != 0 {
+				t.Fatalf("%#v", *msg)
+			}
+			if in != nil {
+				t.Fatalf("%#v", in)
+			}
+			if out != nil {
+				t.Fatalf("%#v", out)
+			}
+		}()
+		in, out, err := w.Send(&Message{Name: "print", Data: "hello world"}, 0)
 		if err != nil {
 			t.Fatal(err)
-		}
-		if msg.Data != "hello world" {
-			t.Fatalf("%#v", *msg)
-		}
-		if msg.Name != "print" {
-			t.Fatalf("%#v", *msg)
-		}
-		if len(msg.Args) != 0 {
-			t.Fatalf("%#v", *msg)
 		}
 		if in != nil {
 			t.Fatalf("%#v", in)
@@ -29,21 +39,5 @@ func TestSimpleSend(t *testing.T) {
 		if out != nil {
 			t.Fatalf("%#v", out)
 		}
-		close(onRcv)
-	}()
-	in, out, err := w.Send(&Message{Name: "print", Data: "hello world"}, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if in != nil {
-		t.Fatalf("%#v", in)
-	}
-	if out != nil {
-		t.Fatalf("%#v", out)
-	}
-	select {
-	case <-onTimeout:
-		t.Fatalf("timeout")
-	case <-onRcv:
-	}
+	})
 }
