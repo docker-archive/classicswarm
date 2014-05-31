c5b8976409a4db82c1ccf22cf19d126b71949f61
Solomon Hykes <solomon@docker.com>
2014-05-31 00:33:53 +0000

    [WIP] switch to Beam

--- backends/backends.go
+++ backends/backends.go
@@ -1,7 +1,12 @@
 package backends
 
 import (
-	"github.com/dotcloud/docker/engine"
+	"fmt"
+	"github.com/docker/beam"
+	beamutils "github.com/docker/beam/utils"
+	"strings"
+	"sync"
+	"time"
 )
 
 // New returns a new engine, with all backends
@@ -10,13 +15,58 @@ import (
 // engine, named after the desired backend.
 //
 // Example: `New().Job("debug").Run()`
-func New() *engine.Engine {
-	back := engine.New()
-	back.Logging = false
-	// Register all backends here
-	Debug().Install(back)
-	Simulator().Install(back)
-	Forward().Install(back)
-	CloudBackend().Install(back)
-	return back
+func New() beam.Sender {
+	backends := beamutils.NewHub()
+	backends.RegisterName("debug", func(msg *beam.Message, in beam.Receiver, out beam.Sender, next beam.Sender) (bool, error) {
+		backends.RegisterTask(func(r beam.Receiver, w beam.Sender) error {
+			for {
+				msg, msgr, msgw, err := r.Receive(beam.R | beam.W)
+				if err != nil {
+					return err
+				}
+				fmt.Printf("[DEBUG] %s %s\n", msg.Name, strings.Join(msg.Args, " "))
+				// FIXME: goroutine?
+				splice(w, msg, msgr, msgw)
+			}
+		})
+		return true, nil
+	})
+	backends.RegisterName("fakeclient", func(msg *beam.Message, in beam.Receiver, out beam.Sender, next beam.Sender) (bool, error) {
+		backends.RegisterTask(func(r beam.Receiver, w beam.Sender) error {
+			for {
+				time.Sleep(1 * time.Second)
+				_, _, err := w.Send(&beam.Message{Name: "log", Args: []string{"fake client reporting for duty"}}, 0)
+				if err != nil {
+					return err
+				}
+				containersR, _, err := w.Send(&beam.Message{Name: "containers"}, beam.R)
+				if err != nil {
+					return err
+				}
+				go beamutils.Copy(beamutils.NopSender{}, containersR)
+			}
+		})
+		return true, nil
+	})
+	return backends
+}
+
+func splice(dst beam.Sender, msg *beam.Message, r beam.Receiver, w beam.Sender) error {
+	dstR, dstW, err := dst.Send(msg, beam.R|beam.W)
+	if err != nil {
+		return err
+	}
+	defer dstW.Close()
+	var tasks sync.WaitGroup
+	_copy := func(dst beam.Sender, src beam.Receiver) {
+		tasks.Add(1)
+		go func() {
+			defer tasks.Done()
+			beamutils.Copy(dst, src)
+		}()
+	}
+	_copy(dstW, r)
+	_copy(w, dstR)
+	tasks.Wait()
+	return nil
 }
