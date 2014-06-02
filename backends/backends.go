3243ae084b7a022fc12b652b262ecf46524d7561
Solomon Hykes <solomon@docker.com>
2014-06-02 07:14:06 +0000

    Move each backend to a separate file
    Signed-off-by: Solomon Hykes <solomon@docker.com>

--- backends/backends.go
+++ backends/backends.go
@@ -1,10 +1,7 @@
 package backends
 
 import (
-	"fmt"
 	"github.com/docker/libswarm/beam"
-	"strings"
-	"time"
 )
 
 // New returns a new engine, with all backends
@@ -20,49 +17,4 @@ func New() *beam.Object {
 	backends.Bind("fakeclient", FakeClient())
 	backends.Bind("forward", Forward())
 	return beam.Obj(backends)
-}
-
-func Debug() beam.Sender {
-	backend := beam.NewServer()
-	backend.OnSpawn(beam.Handler(func(ctx *beam.Message) error {
-		instance := beam.Task(func(in beam.Receiver, out beam.Sender) {
-			fmt.Printf("debug backend!")
-			for {
-				msg, err := in.Receive(beam.Ret)
-				if err != nil {
-					fmt.Printf("debug receive: %v", err)
-					return
-				}
-				fmt.Printf("[DEBUG] %s %s\n", msg.Verb, strings.Join(msg.Args, " "))
-				if _, err := out.Send(msg); err != nil {
-					fmt.Printf("debug send: %v", err)
-					return
-				}
-			}
-		})
-		_, err := ctx.Ret.Send(&beam.Message{Verb: beam.Ack, Ret: instance})
-		return err
-	}))
-	return backend
-}
-
-func FakeClient() beam.Sender {
-	backend := beam.NewServer()
-	backend.OnSpawn(beam.Handler(func(ctx *beam.Message) error {
-		// Instantiate a new fakeclient instance
-		instance := beam.Task(func(in beam.Receiver, out beam.Sender) {
-			fmt.Printf("fake client!\n")
-			defer fmt.Printf("end of fake client!\n")
-			o := beam.Obj(out)
-			o.Log("fake client starting")
-			defer o.Log("fake client terminating")
-			for {
-				time.Sleep(1 * time.Second)
-				o.Log("fake client heartbeat!")
-			}
-		})
-		_, err := ctx.Ret.Send(&beam.Message{Verb: beam.Ack, Ret: instance})
-		return err
-	}))
-	return backend
 }
---
+++ backends/debug.go
@@ -0,0 +1,32 @@
+package backends
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/docker/libswarm/beam"
+)
+
+func Debug() beam.Sender {
+	backend := beam.NewServer()
+	backend.OnSpawn(beam.Handler(func(ctx *beam.Message) error {
+		instance := beam.Task(func(in beam.Receiver, out beam.Sender) {
+			fmt.Printf("debug backend!")
+			for {
+				msg, err := in.Receive(beam.Ret)
+				if err != nil {
+					fmt.Printf("debug receive: %v", err)
+					return
+				}
+				fmt.Printf("[DEBUG] %s %s\n", msg.Verb, strings.Join(msg.Args, " "))
+				if _, err := out.Send(msg); err != nil {
+					fmt.Printf("debug send: %v", err)
+					return
+				}
+			}
+		})
+		_, err := ctx.Ret.Send(&beam.Message{Verb: beam.Ack, Ret: instance})
+		return err
+	}))
+	return backend
+}
