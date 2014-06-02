305c0511cfddf7f75f42adcd6f3b9618eee53b90
Solomon Hykes <solomon@docker.com>
2014-06-02 09:10:52 +0000

    Simple fork-exec backend
    Signed-off-by: Solomon Hykes <solomon@docker.com>
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>
    Signed-off-by: Aanand Prasad <aanand.prasad@gmail.com>

--- swarmd/swarmd.go
+++ swarmd/swarmd.go
@@ -42,37 +42,37 @@ func cmdDaemon(c *cli.Context) {
 		fmt.Println(strings.Join(names, "\n"))
 		return
 	}
-	var previousInstanceIn beam.Receiver
-	for _, backendArg := range c.Args() {
+	var previousInstanceR beam.Receiver
+	// FIXME: refactor into a Pipeline
+	for idx, backendArg := range c.Args() {
 		bName, bArgs, err := parseCmd(backendArg)
 		if err != nil {
 			Fatalf("parse: %v", err)
 		}
-		fmt.Printf("---> Loading backend '%s'\n", strings.Join(append([]string{bName}, bArgs...), " "))
 		_, backend, err := back.Attach(bName)
 		if err != nil {
 			Fatalf("%s: %v\n", bName, err)
 		}
-		fmt.Printf("---> Spawning\n")
 		instance, err := backend.Spawn(bArgs...)
 		if err != nil {
 			Fatalf("spawn %s: %v\n", bName, err)
 		}
-		fmt.Printf("---> Attaching\n")
-		instanceIn, instanceOut, err := instance.Attach("")
+		instanceR, instanceW, err := instance.Attach("")
 		if err != nil {
 			Fatalf("attach: %v", err)
 		}
-		fmt.Printf("---> Starting\n")
+		go func(r beam.Receiver, w beam.Sender, idx int) {
+			if r != nil {
+				beam.Copy(w, r)
+			}
+			w.Close()
+		}(previousInstanceR, instanceW, idx)
 		if err := instance.Start(); err != nil {
 			Fatalf("start: %v", err)
 		}
-		if previousInstanceIn != nil {
-			go beam.Copy(instanceOut, previousInstanceIn)
-		}
-		previousInstanceIn = instanceIn
+		previousInstanceR = instanceR
 	}
-	_, err := beam.Copy(app, previousInstanceIn)
+	_, err := beam.Copy(app, previousInstanceR)
 	if err != nil {
 		Fatalf("copy: %v", err)
 	}
