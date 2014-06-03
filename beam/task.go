52d57ce794cd5a9b85216acf067e08bb84c055a1
Solomon Hykes <solomon@docker.com>
2014-06-03 00:02:23 +0000

    beam.Task: plumbing for a long running task
    Signed-off-by: Solomon Hykes <solomon@docker.com>

---
+++ beam/task.go
@@ -0,0 +1,39 @@
+package beam
+
+import (
+	"fmt"
+	"sync"
+)
+
+func Task(f func(in Receiver, out Sender)) Sender {
+	var running bool
+	var l sync.RWMutex
+	inR, inW := Pipe()
+	outR, outW := Pipe()
+	obj := NewServer()
+	obj.OnAttach(Handler(func(msg *Message) error {
+		msg.Ret.Send(&Message{Name: "ack", Ret: inW})
+		go func() {
+			fmt.Printf("copying task output from %#v to %#v\n", outR, msg.Ret)
+			defer fmt.Printf("(DONE) copying task output from %#v to %#v\n", outR, msg.Ret)
+			Copy(msg.Ret, outR)
+			msg.Ret.Close()
+		}()
+		return nil
+	}))
+	obj.OnStart(Handler(func(msg *Message) error {
+		l.RLock()
+		r := running
+		l.RUnlock()
+		if r {
+			return fmt.Errorf("already running")
+		}
+		l.Lock()
+		go f(inR, outW)
+		running = true
+		l.Unlock()
+		msg.Ret.Send(&Message{Name: "ack"})
+		return nil
+	}))
+	return obj
+}
