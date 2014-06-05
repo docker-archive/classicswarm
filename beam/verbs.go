3271c03bd6cae3afbe67c3f13e7cf3743f7f838e
Ben Firshman <ben@firshman.co.uk>
2014-06-05 12:38:54 -0700

    Add string representation of verb enum
    Docker-DCO-1.1-Signed-off-by: Ben Firshman <ben@firshman.co.uk> (github: bfirsh)

--- beam/verbs.go
+++ beam/verbs.go
@@ -17,3 +17,35 @@ const (
 	Stop
 	Watch
 )
+
+func (v Verb) String() string {
+	switch v {
+	case Ack:
+		return "Ack"
+	case Attach:
+		return "Attach"
+	case Connect:
+		return "Connect"
+	case Error:
+		return "Error"
+	case File:
+		return "File"
+	case Get:
+		return "Get"
+	case Log:
+		return "Log"
+	case Ls:
+		return "Ls"
+	case Set:
+		return "Set"
+	case Spawn:
+		return "Spawn"
+	case Start:
+		return "Start"
+	case Stop:
+		return "Stop"
+	case Watch:
+		return "Watch"
+	}
+	return ""
+}
