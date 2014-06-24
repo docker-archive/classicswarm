5503154e36fdc1e6bb59bff57576d6430d7eea16
Aaron Feng <aaron@forty9ten.com>
2014-06-24 01:41:26 -0400

    use go lib instead of shell out
    Docker-DCO-1.1-Signed-off-by: Aaron Feng aaron.feng@gmail.com (github: aaronfeng)

--- backends/ssh.go
+++ backends/ssh.go
@@ -1,17 +1,38 @@
 package backends
 
 import (
+  "io"
   "os"
-  "os/exec"
   "os/signal"
   "syscall"
+  "fmt"
+  "net"
+  "errors"
+  "io/ioutil"
+  "github.com/codegangsta/cli"
+  "code.google.com/p/go.crypto/ssh"
   "github.com/docker/libswarm/beam"
 )
 
+var (
+  quit          chan bool
+  sshConn       *ssh.Client
+  localListener net.Listener
+  remoteConn    net.Conn
+)
+
+type tunnelConfig struct {
+  user string
+  host string
+  port int
+  key  string
+  localPort int
+  remotePort int
+}
+
 type sshClient struct {
   server *beam.Server
-  args   []string
-  tunnel *os.Process
+  tunnelConfig *tunnelConfig
 }
 
 func (c *sshClient) attach(ctx *beam.Message) error {
@@ -20,45 +41,89 @@ func (c *sshClient) attach(ctx *beam.Message) error {
   return nil
 }
 
+func (c *sshClient) loadSshSigner() (ssh.Signer, error) {
+  data, err := ioutil.ReadFile(c.tunnelConfig.key)
+	if err != nil {
+    return nil, err
+	}
+
+  key, err := ssh.ParseRawPrivateKey(data)
+	if err != nil {
+    return nil, err
+	}
+
+	signer, err := ssh.NewSignerFromKey(key)
+	if err != nil {
+    return nil, err
+	}
+  return signer, nil
+}
+
+func (c *sshClient) newSshConfig(signer ssh.Signer) (*ssh.ClientConfig) {
+  sshConfig := ssh.ClientConfig {
+    User: c.tunnelConfig.user,
+    Auth: []ssh.AuthMethod {
+      ssh.PublicKeys(signer),
+    },
+  }
+  return &sshConfig
+}
+
 func (c *sshClient) start(ctx *beam.Message) error {
-  defaultOptions := []string {
-	  "-o", "PasswordAuthentication=no",
-	  "-o", "LogLevel=quiet",
-	  "-o", "UserKnownHostsFile=/dev/null",
-	  "-o", "CheckHostIP=no",
-	  "-o", "StrictHostKeyChecking=no",
-	  "-A",
-		"-N",
-	  "-f",
-  }
-
-  var allOptions []string
-  if len(c.args) > 0 {
-    allOptions = append(defaultOptions, c.args...)
-  } else {
-    allOptions = append(defaultOptions, ctx.Args...)
-  }
-
-	cmd := exec.Command("ssh", allOptions...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
+  signer, err := c.loadSshSigner()
+	if err != nil {
 		return err
 	}
 
-  c.tunnel = cmd.Process
+  sshConfig := c.newSshConfig(signer)
 
-  ctx.Ret.Send(&beam.Message{Verb: beam.Ack, Ret: c.server})
+  sshConn, err = ssh.Dial("tcp", fmt.Sprintf("%s:%d",
+                                             c.tunnelConfig.host,
+                                             c.tunnelConfig.port),
+                                             sshConfig)
+	if err != nil {
+		return err
+	}
+
+  localListener, err = net.Listen("tcp", fmt.Sprintf("localhost:%d",
+                                                  c.tunnelConfig.localPort))
+	if err != nil {
+		return err
+	}
+
+	for {
+		incomingConn, err := localListener.Accept()
+		if err != nil {
+			return err
+		}
+
+		go func() {
+      remoteConn, err = sshConn.Dial("tcp", fmt.Sprintf("localhost:%d",
+                                                        c.tunnelConfig.remotePort))
+			if err != nil {
+				return
+			}
+
+			go copyData(incomingConn, remoteConn)
+			go copyData(remoteConn, incomingConn)
+
+		}()
+
+	}
   return nil
 }
 
-func (c *sshClient) Close() {
-  if c.tunnel != nil {
-    c.tunnel.Kill()
-	  c.tunnel.Wait()
-		c.tunnel = nil
-  }
+func copyData(in net.Conn, out net.Conn) {
+	buf := make([]byte, 10240)
+	for {
+		n, err := in.Read(buf)
+		if io.EOF == err {
+			return
+		} else if err != nil {
+			return
+		}
+		out.Write(buf[:n])
+	}
 }
 
 func handleSignal(client *sshClient) {
@@ -67,25 +132,79 @@ func handleSignal(client *sshClient) {
   signal.Notify(c, syscall.SIGTERM)
   go func() {
       <-c
-      client.Close()
+			sshConn.Close()
+			localListener.Close()
+      if (remoteConn != nil) {
+			  remoteConn.Close()
+      }
       os.Exit(0)
   }()
+}
+
+func parseArgs(args []string) (*tunnelConfig, error) {
+  app := cli.NewApp()
+  app.Flags = []cli.Flag {
+    cli.StringFlag{"user",         "",     "REQUIRED - ssh user"},
+    cli.StringFlag{"host",         "",     "REQUIRED - ssh host"},
+    cli.StringFlag{"key",          "",     "REQUIRED - ssh key - full path"},
+    cli.IntFlag   {"port",         22,     "OPTIONAL - ssh port"},
+    cli.IntFlag   {"local_port",   4910,   "OPTIONAL - ssh tunnel local port"},
+    cli.IntFlag   {"remote_port",  4243,   "OPTIONAL - ssh tunnel remote port"},
+  }
+
+  var config *tunnelConfig
+  var showHelp = false
+
+  app.Action = func(c *cli.Context) {
+    config = &tunnelConfig {
+      user:       c.String("user"),
+      host:       c.String("host"),
+      port:       c.Int("port"),
+      key:        c.String("key"),
+      localPort:  c.Int("local_port"),
+      remotePort: c.Int("remote_port"),
+    }
+  }
+
+  app.Run(append([]string{"ssh"}, args...))
+
+  if len(args) == 0    || // no args passed
+     config == nil     || // help command passed
+     config.host == "" ||
+     config.key  == "" ||
+     config.user == "" {
+
+    showHelp = true
+  }
+
+  if showHelp {
+    app.Run(append([]string{"ssh"}, "help"))
+    return nil, errors.New("help")
+  }
+
+  return config, nil
 }
 
 func Ssh() beam.Sender {
   backend := beam.NewServer()
   backend.OnSpawn(beam.Handler(func(ctx *beam.Message) error {
-    client := &sshClient{beam.NewServer(), ctx.Args, nil}
+
+    config, err := parseArgs(ctx.Args)
+
+    if err != nil {
+      return err
+    }
+
+    client := &sshClient{beam.NewServer(), config}
 
     client.server.OnStart(beam.Handler(client.start))
     client.server.OnAttach(beam.Handler(client.attach))
 
     handleSignal(client)
-    _, err := ctx.Ret.Send(&beam.Message{Verb: beam.Ack, Ret: client.server})
+    _, err = ctx.Ret.Send(&beam.Message{Verb: beam.Ack, Ret: client.server})
 
     return err
   }))
 
   return backend
 }
-
