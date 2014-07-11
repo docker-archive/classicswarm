3c0aa505334584e98e9441864c093c6d132a2a81
Aanand Prasad <aanand.prasad@gmail.com>
2014-07-11 14:42:07 -0700

    Use new libchan interface
    Signed-off-by: Aanand Prasad <aanand.prasad@gmail.com>

--- message.go
+++ message.go
@@ -1,29 +1,33 @@
 package libswarm
 
 import (
-	"github.com/docker/libchan"
-	"github.com/docker/libchan/data"
+	"github.com/dmcgowan/libchan"
 
 	"fmt"
-	"os"
+	"io"
 )
 
 type Message struct {
 	Verb
 	Args []string
 	Ret  Sender
-	Att  *os.File
+	Att  io.ReadWriteCloser
 }
 
 type Sender interface {
 	Send(msg *Message) (Receiver, error)
 	Close() error
-	Unwrap() libchan.Sender
 }
 
 type Receiver interface {
 	Receive(mode int) (*Message, error)
-	Unwrap() libchan.Receiver
+}
+
+type internalMessage struct {
+	Verb
+	Args []string
+	Ret  libchan.Sender
+	Att  io.ReadWriteCloser
 }
 
 type senderWrapper struct {
@@ -35,15 +39,47 @@ func WrapSender(s libchan.Sender) Sender {
 }
 
 func (s *senderWrapper) Send(msg *Message) (Receiver, error) {
-	recv, err := s.Sender.Send(msg.LibchanMessage())
-	if err != nil {
-		return nil, err
-	}
-	return WrapReceiver(recv), err
-}
+	var rcvr Receiver = NopReceiver{}
 
-func (s *senderWrapper) Unwrap() libchan.Sender {
-	return s.Sender
+	imsg := &internalMessage{
+		Verb: msg.Verb,
+		Args: msg.Args,
+	}
+
+	if msg.Ret != nil {
+		thisEnd, otherEnd, err := s.Sender.CreateNestedReceiver()
+		if err != nil {
+			return nil, err
+		}
+
+		imsg.Ret = otherEnd
+
+		if RetPipe.Equals(msg.Ret) {
+			rcvr = &receiverWrapper{thisEnd}
+		} else {
+			go Copy(msg.Ret, &receiverWrapper{thisEnd})
+		}
+	}
+
+	if msg.Att != nil {
+		byteStream, err := s.Sender.CreateByteStream()
+		if err != nil {
+			return nil, err
+		}
+
+		imsg.Att = byteStream
+
+		go func() {
+			io.Copy(byteStream, msg.Att)
+			byteStream.Close()
+		}()
+		go func() {
+			io.Copy(msg.Att, byteStream)
+			msg.Att.Close()
+		}()
+	}
+
+	return rcvr, s.Sender.Send(imsg)
 }
 
 type receiverWrapper struct {
@@ -55,115 +91,113 @@ func WrapReceiver(r libchan.Receiver) Receiver {
 }
 
 func (r *receiverWrapper) Receive(mode int) (*Message, error) {
-	lcm, err := r.Receiver.Receive(mode)
-	if err != nil {
+	imsg := &internalMessage{}
+	if err := r.Receiver.Receive(imsg); err != nil {
 		return nil, err
 	}
-	return DecodeLibchanMessage(lcm)
-}
-
-func (r *receiverWrapper) Unwrap() libchan.Receiver {
-	return r.Receiver
-}
-
-type senderUnwrapper struct {
-	Sender
-}
-
-func (su *senderUnwrapper) Send(lcm *libchan.Message) (libchan.Receiver, error) {
-	msg, err := DecodeLibchanMessage(lcm)
-	if err != nil {
-		return nil, err
+	var ret Sender
+	if imsg.Ret == nil {
+		ret = NopSender{}
+	} else {
+		ret = &senderWrapper{imsg.Ret}
 	}
-	recv, err := su.Sender.Send(msg)
-	if err != nil {
-		return nil, err
+	if mode&Ret == 0 {
+		if err := ret.Close(); err != nil {
+			return nil, err
+		}
 	}
-	return &receiverUnwrapper{recv}, nil
-}
-
-type receiverUnwrapper struct {
-	Receiver
-}
-
-func (ru *receiverUnwrapper) Receive(mode int) (*libchan.Message, error) {
-	msg, err := ru.Receiver.Receive(mode)
-	if err != nil {
-		return nil, err
+	msg := &Message{
+		Verb: imsg.Verb,
+		Args: imsg.Args,
+		Ret:  ret,
+		Att:  imsg.Att,
 	}
-	return msg.LibchanMessage(), nil
+	return msg, nil
 }
 
-func Pipe() (Receiver, Sender) {
+func Pipe() (*receiverWrapper, *senderWrapper) {
 	r, s := libchan.Pipe()
-	return WrapReceiver(r), WrapSender(s)
+	return &receiverWrapper{r}, &senderWrapper{s}
 }
 
-func Copy(s Sender, r Receiver) (int, error) {
-	return libchan.Copy(s.Unwrap(), r.Unwrap())
-}
-
-func Handler(h func(msg *Message) error) Sender {
-	lch := libchan.Handler(func(lcm *libchan.Message) {
-		ret := WrapSender(lcm.Ret)
-		msg, err := DecodeLibchanMessage(lcm)
+func Copy(dst Sender, src Receiver) (int, error) {
+	var n int
+	for {
+		msg, err := src.Receive(Ret)
+		if err == io.EOF {
+			return n, nil
+		}
 		if err != nil {
-			ret.Send(&Message{Verb: Error, Args: []string{err.Error()}})
+			return n, err
 		}
-		if err = h(msg); err != nil {
-			ret.Send(&Message{Verb: Error, Args: []string{err.Error()}})
+		if _, err := dst.Send(msg); err != nil {
+			return n, err
 		}
-	})
-	return WrapSender(lch)
+		n++
+	}
 }
 
-var RetPipe = WrapSender(libchan.RetPipe)
-var Ret = libchan.Ret
+type Handler func(msg *Message) error
+
+func (h Handler) Send(msg *Message) (Receiver, error) {
+	var ret Receiver
+	if RetPipe.Equals(msg.Ret) {
+		ret, msg.Ret = Pipe()
+	}
+	go func() {
+		if msg.Ret == nil {
+			msg.Ret = NopSender{}
+		}
+		h(msg)
+		msg.Ret.Close()
+	}()
+	return ret, nil
+}
+
+func (h Handler) Close() error {
+	return fmt.Errorf("can't close a Handler")
+}
+
+func Repeater(payload *Message) Sender {
+	return Handler(func(msg *Message) error {
+		msg.Ret.Send(payload)
+		return nil
+	})
+}
 
 var notImplementedMsg = &Message{Verb: Error, Args: []string{"not implemented"}}
-var NotImplemented = WrapSender(libchan.Repeater(notImplementedMsg.LibchanMessage()))
+var NotImplemented = Repeater(notImplementedMsg)
 
-func DecodeLibchanMessage(lcm *libchan.Message) (*Message, error) {
-	decoded, err := data.Decode(string(lcm.Data))
-	if err != nil {
-		return nil, err
-	}
-	verbList, exists := decoded["verb"]
-	if !exists {
-		return nil, fmt.Errorf("No 'verb' key found in message data: %s", lcm.Data)
-	}
-	if len(verbList) != 1 {
-		return nil, fmt.Errorf("Expected exactly one verb, got %d: %#v", len(verbList), verbList)
-	}
-	verb, err := VerbFromString(verbList[0])
-	if err != nil {
-		return nil, err
-	}
-	args, exists := decoded["args"]
-	if !exists {
-		return nil, fmt.Errorf("No 'args' key found in message data: %s", lcm.Data)
-	}
-	return &Message{
-		Verb: verb,
-		Args: args,
-		Ret:  WrapSender(lcm.Ret),
-		Att:  lcm.Fd,
-	}, nil
+const (
+	Ret int = 1 << iota
+	// FIXME: use an `Att` flag to auto-close attachments by default
+)
+
+type retPipe struct {
+	NopSender
 }
 
-func (m *Message) LibchanMessage() *libchan.Message {
-	encoded := data.Empty().
-		Set("verb", m.Verb.String()).
-		Set("args", m.Args...)
+var RetPipe = retPipe{}
 
-	var ret libchan.Sender
-	if m.Ret != nil {
-		ret = m.Ret.Unwrap()
+func (r retPipe) Equals(val Sender) bool {
+	if rval, ok := val.(retPipe); ok {
+		return rval == r
 	}
+	return false
+}
 
-	return &libchan.Message{
-		Data: []byte(encoded),
-		Ret:  ret,
-		Fd:   m.Att,
-	}
+type NopSender struct{}
+
+func (s NopSender) Send(msg *Message) (Receiver, error) {
+	return NopReceiver{}, nil
+}
+
+func (s NopSender) Close() error {
+	return nil
+}
+
+type NopReceiver struct{}
+
+func (r NopReceiver) Receive(mode int) (*Message, error) {
+	return nil, io.EOF
 }
