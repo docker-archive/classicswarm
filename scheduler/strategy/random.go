004450a910443fbf60bbc7d3dc5c34d5b3148a13
Andrea Luzzardi <aluzzardi@gmail.com>
2014-11-17 13:30:47 -0800

    Make the random placement strategy truly random.
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- scheduler/strategy/random.go
+++ scheduler/strategy/random.go
@@ -2,18 +2,27 @@ package strategy
 
 import (
 	"errors"
+	"math/rand"
+	"time"
 
 	"github.com/docker/libcluster"
 	"github.com/samalba/dockerclient"
 )
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
 
 // Randomly place the container into the cluster.
 type RandomPlacementStrategy struct {
 }
 
 func (p *RandomPlacementStrategy) PlaceContainer(config *dockerclient.ContainerConfig, nodes []*libcluster.Node) (*libcluster.Node, error) {
-	for _, node := range nodes {
-		return node, nil
+	n := rand.Intn(len(nodes))
+	for i, node := range nodes {
+		if i == n {
+			return node, nil
+		}
 	}
 	return nil, errors.New("No nodes running in the cluster")
 }
