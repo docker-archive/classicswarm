bf04e484f9f6df34a6d5718526bbb9d242765d3c
Victor Vieux <vieux@docker.com>
2014-11-21 22:11:36 +0000

    add contrib folder

---
+++ contrib/redmon.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# start a redis server
+docker run -d -P --name redis redis
+
+# start redmon and point it to the redis container's IP:port
+docker run -d -P --name redmon vieux/redmon -r redis://`docker port redis 6379`
+
+echo "http://`docker port redmon 4567`"
