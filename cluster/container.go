57f566dacd82e6d6dd169498660725c0428a9448
Victor Vieux <vieux@docker.com>
2014-12-13 00:23:52 +0000

    remove useless functs
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- cluster/container.go
+++ cluster/container.go
@@ -12,27 +12,3 @@ type Container struct {
 func (c *Container) Node() *Node {
 	return c.node
 }
-
-func (c *Container) Start() error {
-	return c.node.client.StartContainer(c.Id, nil)
-}
-
-func (c *Container) Kill(signal string) error {
-	return c.node.client.KillContainer(c.Id, signal)
-}
-
-func (c *Container) Stop() error {
-	return c.node.client.StopContainer(c.Id, 8)
-}
-
-func (c *Container) Restart(timeout int) error {
-	return c.node.client.RestartContainer(c.Id, timeout)
-}
-
-func (c *Container) Pause() error {
-	return c.node.client.PauseContainer(c.Id)
-}
-
-func (c *Container) Unpause() error {
-	return c.node.client.UnpauseContainer(c.Id)
-}
