e0461c754985a7998025a64a41715fa1f2d99913
JChien <jeffchien13@gmail.com>
2014-12-17 15:28:42 +0800

    fix readme and remove swarm prefix
    Signed-off-by: JChien <jeffchien13@gmail.com>

--- discovery/consul/consul_test.go
+++ discovery/consul/consul_test.go
@@ -9,5 +9,5 @@ import (
 func TestInitialize(t *testing.T) {
 	discovery := &ConsulDiscoveryService{}
 	discovery.Initialize("127.0.0.1:8500/path", 0)
-	assert.Equal(t, discovery.prefix, "swarm/path/")
+	assert.Equal(t, discovery.prefix, "path/")
 }
