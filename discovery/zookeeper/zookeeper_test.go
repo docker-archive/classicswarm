195ba02473140a6d7a422dc8d76a46a35703c98e
Chanwit Kaewkasi <chanwit@gmail.com>
2015-01-06 18:51:02 +0700

    add a unit test for zk discovery
    Signed-off-by: Chanwit Kaewkasi <chanwit@gmail.com>

---
+++ discovery/zookeeper/zookeeper_test.go
@@ -0,0 +1,16 @@
+package zookeeper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitialize(t *testing.T) {
+	discovery := &ZkDiscoveryService{}
+	assert.Error(t, discovery.Initialize("127.0.0.1/path", 0))
+	assert.Equal(t, discovery.path, "/path")
+
+	assert.Error(t, discovery.Initialize("127.0.0.1,127.0.0.2,127.0.0.3/path", 0))
+	assert.Equal(t, discovery.path, "/path")
+}
