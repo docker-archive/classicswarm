509dcd362f8b4d2c370500b67b8eb95f7393e8d2
mountkin <mountkin@gmail.com>
2015-01-07 12:43:13 +0800

    add test cases for node.Create method
    Signed-off-by: mountkin <mountkin@gmail.com>

--- cluster/node_test.go
+++ cluster/node_test.go
@@ -131,3 +131,58 @@ func TestNodeState(t *testing.T) {
 
 	client.Mock.AssertExpectations(t)
 }
+
+func TestCreateContainer(t *testing.T) {
+	var (
+		config = &dockerclient.ContainerConfig{
+			Image:     "busybox",
+			CpuShares: 512,
+			Cmd:       []string{"date"},
+			Tty:       false,
+		}
+		node   = NewNode("test")
+		client = mockclient.NewMockClient()
+	)
+
+	client.On("Info").Return(mockInfo, nil)
+	client.On("StartMonitorEvents", mock.Anything, mock.Anything).Return()
+	client.On("ListContainers", true, false, "").Return([]dockerclient.Container{}, nil).Once()
+	assert.NoError(t, node.connectClient(client))
+	assert.True(t, node.IsConnected())
+
+	mockConfig := *config
+	mockConfig.CpuShares = config.CpuShares * mockInfo.NCPU
+
+	// Everything is ok
+	name := "test1"
+	id := "id1"
+	client.On("CreateContainer", &mockConfig, name).Return(id, nil).Once()
+	client.On("ListContainers", true, false, fmt.Sprintf(`{"id":[%q]}`, id)).Return([]dockerclient.Container{{Id: id}}, nil).Once()
+	client.On("InspectContainer", id).Return(&dockerclient.ContainerInfo{Config: config}, nil).Once()
+	container, err := node.Create(config, name, false)
+	assert.Nil(t, err)
+	assert.Equal(t, container.Id, id)
+	assert.Len(t, node.Containers(), 1)
+
+	// Image not found, pullImage == false
+	name = "test2"
+	mockConfig.CpuShares = config.CpuShares * mockInfo.NCPU
+	client.On("CreateContainer", &mockConfig, name).Return("", dockerclient.ErrNotFound).Once()
+	container, err = node.Create(config, name, false)
+	assert.Equal(t, err, dockerclient.ErrNotFound)
+	assert.Nil(t, container)
+
+	// Image not found, pullImage == true, and the image can be pulled successfully
+	name = "test3"
+	id = "id3"
+	mockConfig.CpuShares = config.CpuShares * mockInfo.NCPU
+	client.On("PullImage", config.Image, mock.Anything).Return(nil).Once()
+	client.On("CreateContainer", &mockConfig, name).Return("", dockerclient.ErrNotFound).Once()
+	client.On("CreateContainer", &mockConfig, name).Return(id, nil).Once()
+	client.On("ListContainers", true, false, fmt.Sprintf(`{"id":[%q]}`, id)).Return([]dockerclient.Container{{Id: id}}, nil).Once()
+	client.On("InspectContainer", id).Return(&dockerclient.ContainerInfo{Config: config}, nil).Once()
+	container, err = node.Create(config, name, true)
+	assert.Nil(t, err)
+	assert.Equal(t, container.Id, id)
+	assert.Len(t, node.Containers(), 2)
+}
