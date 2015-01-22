45728e0d0f96a112b6ce6f37aa8c989a714f617e
Victor Vieux <vieux@docker.com>
2015-01-22 19:40:36 +0000

    add consul to usage
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- help.go
+++ help.go
@@ -13,6 +13,7 @@ DESCRIPTION:
 ARGUMENTS:
    discovery{{printf "\t"}}discovery service to use [$SWARM_DISCOVERY]
             {{printf "\t"}} * token://<token>
+            {{printf "\t"}} * consul://<ip1>,<ip2>/<path>
             {{printf "\t"}} * etcd://<ip1>,<ip2>/<path>
             {{printf "\t"}} * file://path/to/file
             {{printf "\t"}} * zk://<ip1>,<ip2>/<path>
