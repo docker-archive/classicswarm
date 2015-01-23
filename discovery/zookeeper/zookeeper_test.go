54e540f0adbacb979f402a8e720da52172db564d
Chanwit Kaewkasi <chanwit@gmail.com>
2015-01-23 14:33:47 +0700

    cleanup to support fullpath creation for zookeeper
    Signed-off-by: Chanwit Kaewkasi <chanwit@gmail.com>

--- discovery/zookeeper/zookeeper_test.go
+++ discovery/zookeeper/zookeeper_test.go
@@ -13,10 +13,13 @@ func TestInitialize(t *testing.T) {
 	assert.Equal(t, service.Initialize("127.0.0.1", 0).Error(), "invalid format \"127.0.0.1\", missing <path>")
 
 	assert.Error(t, service.Initialize("127.0.0.1/path", 0))
-	assert.Equal(t, service.path, "/path")
+	assert.Equal(t, service.fullpath(), "/path")
 
 	assert.Error(t, service.Initialize("127.0.0.1,127.0.0.2,127.0.0.3/path", 0))
-	assert.Equal(t, service.path, "/path")
+	assert.Equal(t, service.fullpath(), "/path")
+
+	assert.Error(t, service.Initialize("127.0.0.1,127.0.0.2,127.0.0.3/path/sub1/sub2", 0))
+	assert.Equal(t, service.fullpath(), "/path/sub1/sub2")
 }
 
 func TestCreateNodes(t *testing.T) {
