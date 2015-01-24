aa6ec25b2b03c2a0861545b51e71769f6d9a1788
Victor Vieux <vieux@docker.com>
2015-01-24 01:36:39 +0000

    fix panic with no port
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- discovery/zookeeper/zookeeper_test.go
+++ discovery/zookeeper/zookeeper_test.go
@@ -21,8 +21,16 @@ func TestInitialize(t *testing.T) {
 
 func TestCreateNodes(t *testing.T) {
 	service := &ZkDiscoveryService{}
-	assert.Equal(t, service.createNodes(nil), []*discovery.Node{})
-	nodes := service.createNodes([]string{"127.0.0.1", "127.0.0.2"})
-	assert.Equal(t, nodes[0].String(), "127.0.0.1")
-	assert.Equal(t, nodes[1].String(), "127.0.0.2")
+
+	nodes, err := service.createNodes(nil)
+	assert.Equal(t, nodes, []*discovery.Node{})
+	assert.NoError(t, err)
+
+	nodes, err = service.createNodes([]string{"127.0.0.1:2375", "127.0.0.2:2375"})
+	assert.Equal(t, nodes[0].String(), "127.0.0.1:2375")
+	assert.Equal(t, nodes[1].String(), "127.0.0.2:2375")
+	assert.NoError(t, err)
+
+	_, err = service.createNodes([]string{"127.0.0.1", "127.0.0.2"})
+	assert.Error(t, err)
 }
