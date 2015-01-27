23d98f9eb63b5f14a1dfe3fe22647303152ab119
Andrea Luzzardi <aluzzardi@gmail.com>
2015-01-27 12:37:44 -0800

    Add affinity to the redmon demo.
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- contrib/redmon.sh
+++ contrib/redmon.sh
@@ -4,6 +4,6 @@
 docker run -d -P --name redis redis
 
 # start redmon and point it to the redis container's IP:port
-docker run -d -P --name redmon vieux/redmon -r redis://`docker port redis 6379`
+docker run -d -P --name redmon -e affinity:container!=redis vieux/redmon -r redis://`docker port redis 6379`
 
 echo "http://`docker port redmon 4567`"
