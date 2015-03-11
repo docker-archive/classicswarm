af05bfd0baf264f4ccab5063d36b54d8981d0146
Victor Vieux <vieux@docker.com>
2015-03-11 14:29:21 -0700

    fix test
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- scheduler/filter/fakenode_test.go
+++ scheduler/filter/fakenode_test.go
@@ -1,6 +1,9 @@
 package filter
 
-import "github.com/docker/swarm/cluster"
+import (
+	"github.com/docker/swarm/cluster"
+	"github.com/samalba/dockerclient"
+)
 
 type FakeNode struct {
 	id         string
@@ -11,11 +14,12 @@ type FakeNode struct {
 	labels     map[string]string
 }
 
-func (fn *FakeNode) ID() string               { return fn.id }
-func (fn *FakeNode) Name() string             { return fn.name }
-func (fn *FakeNode) IP() string               { return "" }
-func (fn *FakeNode) Addr() string             { return fn.addr }
-func (fn *FakeNode) Images() []*cluster.Image { return fn.images }
+func (fn *FakeNode) DockerClient() dockerclient.Client { return nil }
+func (fn *FakeNode) ID() string                        { return fn.id }
+func (fn *FakeNode) Name() string                      { return fn.name }
+func (fn *FakeNode) IP() string                        { return "" }
+func (fn *FakeNode) Addr() string                      { return fn.addr }
+func (fn *FakeNode) Images() []*cluster.Image          { return fn.images }
 func (fn *FakeNode) Image(id string) *cluster.Image {
 	for _, image := range fn.images {
 		if image.Id == id {
