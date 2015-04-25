3ea93d56b5f411e0a5de922a0a7536fc5a87260b
Victor Vieux <victorvieux@gmail.com>
2015-04-25 01:47:00 -0700

    store constaints and affinities in labels
    Signed-off-by: Victor Vieux <victorvieux@gmail.com>

--- api/handlers.go
+++ api/handlers.go
@@ -195,7 +195,7 @@ func getContainerJSON(c *context, w http.ResponseWriter, r *http.Request) {
 func postContainersCreate(c *context, w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	var (
-		config cluster.ContainerConfig
+		config dockerclient.ContainerConfig
 		name   = r.Form.Get("name")
 	)
 
@@ -209,7 +209,7 @@ func postContainersCreate(c *context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	container, err := c.cluster.CreateContainer(&config, name)
+	container, err := c.cluster.CreateContainer(cluster.BuildContainerConfig(&config), name)
 	if err != nil {
 		httpError(w, err.Error(), http.StatusInternalServerError)
 		return
