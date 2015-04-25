b47e002e6c5a0ca996fa21027f5538d7beb64cd0
Xian Chaobo <xianchaobo@huawei.com>
2015-04-24 21:09:26 -0400

    improve rename and update dockerclient
    Signed-off-by: Xian Chaobo <xianchaobo@huawei.com>

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -43,4 +43,7 @@ type Cluster interface {
 	// FIXME: remove this method
 	// Return a random engine
 	RANDOMENGINE() (*Engine, error)
+
+	// Rename a container
+	Rename(container *Container, newName string) error
 }
