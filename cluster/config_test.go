2e0ab25e9608af9e947525f50e3b6cc08b3bf64b
Andrea Luzzardi <aluzzardi@gmail.com>
2015-04-30 15:35:46 -0700

    cleanup config asserts
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- cluster/config_test.go
+++ cluster/config_test.go
@@ -9,44 +9,44 @@ import (
 
 func TestBuildContainerConfig(t *testing.T) {
 	config := BuildContainerConfig(&dockerclient.ContainerConfig{})
-	assert.Equal(t, len(config.Env), 0)
-	assert.Equal(t, len(config.Labels), 0)
+	assert.Empty(t, config.Env)
+	assert.Empty(t, config.Labels)
 
 	config = BuildContainerConfig(&dockerclient.ContainerConfig{Env: []string{"test=true"}})
-	assert.Equal(t, len(config.Env), 1)
-	assert.Equal(t, len(config.Labels), 0)
+	assert.Len(t, config.Env, 1)
+	assert.Empty(t, config.Labels)
 
 	config = BuildContainerConfig(&dockerclient.ContainerConfig{Env: []string{"constraint:test==true"}})
-	assert.Equal(t, len(config.Env), 0)
-	assert.Equal(t, len(config.Labels), 1)
+	assert.Empty(t, config.Env)
+	assert.Len(t, config.Labels, 1)
 
 	config = BuildContainerConfig(&dockerclient.ContainerConfig{Env: []string{"affinity:container==test"}})
-	assert.Equal(t, len(config.Env), 0)
-	assert.Equal(t, len(config.Labels), 1)
+	assert.Empty(t, config.Env)
+	assert.Len(t, config.Labels, 1)
 
 	config = BuildContainerConfig(&dockerclient.ContainerConfig{Env: []string{"test=true", "constraint:test==true", "affinity:container==test"}})
-	assert.Equal(t, len(config.Env), 1)
-	assert.Equal(t, len(config.Labels), 2)
+	assert.Len(t, config.Env, 1)
+	assert.Len(t, config.Labels, 2)
 }
 
 func TestConstraints(t *testing.T) {
 	config := BuildContainerConfig(&dockerclient.ContainerConfig{})
-	assert.Equal(t, len(config.Constraints()), 0)
+	assert.Empty(t, config.Constraints())
 
 	config = BuildContainerConfig(&dockerclient.ContainerConfig{Env: []string{"constraint:test==true"}})
-	assert.Equal(t, len(config.Constraints()), 1)
+	assert.Len(t, config.Constraints(), 1)
 
 	config = BuildContainerConfig(&dockerclient.ContainerConfig{Env: []string{"test=true", "constraint:test==true", "affinity:container==test"}})
-	assert.Equal(t, len(config.Constraints()), 1)
+	assert.Len(t, config.Constraints(), 1)
 }
 
 func TestAffinities(t *testing.T) {
 	config := BuildContainerConfig(&dockerclient.ContainerConfig{})
-	assert.Equal(t, len(config.Affinities()), 0)
+	assert.Empty(t, config.Affinities())
 
 	config = BuildContainerConfig(&dockerclient.ContainerConfig{Env: []string{"affinity:container==test"}})
-	assert.Equal(t, len(config.Affinities()), 1)
+	assert.Len(t, config.Affinities(), 1)
 
 	config = BuildContainerConfig(&dockerclient.ContainerConfig{Env: []string{"test=true", "constraint:test==true", "affinity:container==test"}})
-	assert.Equal(t, len(config.Affinities()), 1)
+	assert.Len(t, config.Affinities(), 1)
 }
