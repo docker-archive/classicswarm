2666474ec761a0375d2d623e4fa31941476d8b20
Andrea Luzzardi <aluzzardi@gmail.com>
2015-05-01 19:07:14 -0700

    integration tests: Support for running within Docker.
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

---
+++ test/integration/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+# Root directory of Swarm.
+SWARM_ROOT=$(cd ../..; pwd -P)
+
+# Image containing the integration tests environment.
+INTEGRATION_IMAGE=${INTEGRATION_IMAGE:-dockerswarm/swarm-test-env}
+
+# Make sure we upgrade the integration environment.
+#docker pull $INTEGRATION_IMAGE
+
+# Start the integration tests in a Docker container.
+ID=$(docker run -d -t --privileged \
+	-v ${SWARM_ROOT}:/go/src/github.com/docker/swarm \
+	-e "DOCKER_IMAGE=$DOCKER_IMAGE" \
+	-e "DOCKER_VERSION=$DOCKER_VERSION" \
+	-e "STORAGE_DRIVER=$STORAGE_DRIVER" \
+	-e "EXEC_DRIVER=$EXEC_DRIVER" \
+	${INTEGRATION_IMAGE} \
+	./test_runner.sh "$@")
+
+# Clean it up when we exit.
+trap "docker rm -f -v $ID > /dev/null" EXIT
+
+docker logs -f $ID
