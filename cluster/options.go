fc1e42a4ee0ba9d9a85651ad0c406a75bd7c922d
Isabel Jimenez <contact.isabeljimenez@gmail.com>
2015-05-18 18:52:50 -0400

    adding environment variable option when treating flags
    Signed-off-by: Isabel Jimenez <contact.isabeljimenez@gmail.com>

--- cluster/options.go
+++ cluster/options.go
@@ -1,6 +1,7 @@
 package cluster
 
 import (
+	"os"
 	"strconv"
 	"strings"
 )
@@ -9,19 +10,22 @@ import (
 type DriverOpts []string
 
 // String returns a string from the driver options
-func (do DriverOpts) String(key string) (string, bool) {
+func (do DriverOpts) String(key, env string) (string, bool) {
 	for _, opt := range do {
 		kv := strings.SplitN(opt, "=", 2)
 		if kv[0] == key {
 			return kv[1], true
 		}
 	}
+	if env := os.Getenv(env); env != "" {
+		return env, true
+	}
 	return "", false
 }
 
 // Int returns an int64 from the driver options
-func (do DriverOpts) Int(key string) (int64, bool) {
-	if value, ok := do.String(key); ok {
+func (do DriverOpts) Int(key, env string) (int64, bool) {
+	if value, ok := do.String(key, env); ok {
 		v, _ := strconv.ParseInt(value, 0, 64)
 		return v, true
 	}
@@ -29,8 +33,8 @@ func (do DriverOpts) Int(key string) (int64, bool) {
 }
 
 // Uint returns an int64 from the driver options
-func (do DriverOpts) Uint(key string) (uint64, bool) {
-	if value, ok := do.String(key); ok {
+func (do DriverOpts) Uint(key, env string) (uint64, bool) {
+	if value, ok := do.String(key, env); ok {
 		v, _ := strconv.ParseUint(value, 0, 64)
 		return v, true
 	}
@@ -38,8 +42,8 @@ func (do DriverOpts) Uint(key string) (uint64, bool) {
 }
 
 // Float returns a float64 from the driver options
-func (do DriverOpts) Float(key string) (float64, bool) {
-	if value, ok := do.String(key); ok {
+func (do DriverOpts) Float(key, env string) (float64, bool) {
+	if value, ok := do.String(key, env); ok {
 		v, _ := strconv.ParseFloat(value, 64)
 		return v, true
 	}
