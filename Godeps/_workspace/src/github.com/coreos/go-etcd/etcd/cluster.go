376c4ff458df6b8524d145bf2a4feb31fa1b9074
Isabel Jimenez <contact.isabeljimenez@gmail.com>
2015-05-21 15:39:25 -0400

    Godeps for Mesos
    Signed-off-by: Isabel Jimenez <contact.isabeljimenez@gmail.com>

--- Godeps/_workspace/src/github.com/coreos/go-etcd/etcd/cluster.go
+++ Godeps/_workspace/src/github.com/coreos/go-etcd/etcd/cluster.go
@@ -30,8 +30,5 @@ func (cl *Cluster) pick() string { return cl.Machines[cl.picked] }
 
 func (cl *Cluster) updateFromStr(machines string) {
 	cl.Machines = strings.Split(machines, ",")
-	for i := range cl.Machines {
-		cl.Machines[i] = strings.TrimSpace(cl.Machines[i])
-	}
-	cl.picked = rand.Intn(len(cl.Machines))
+	cl.picked = rand.Intn(len(machines))
 }
