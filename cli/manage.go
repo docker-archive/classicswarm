jimenez/swarm

Adding integration tests, decline offers after 'SWARM_MESOS_OFFER_TIMEOUT', Tracking tasks and managing offers, refactoring list of slaves, queue create requests before processing them
Signed-off-by: Isabel Jimenez <contact.isabeljimenez@gmail.com>
Signed-off-by: Victor Vieux <vieux@docker.com>

cli/manage.go
@@ -120,17 +120,18 @@ func manage(c *cli.Context) {
 
 	sched := scheduler.New(s, fs)
 
-	cluster, err := swarm.NewCluster(sched, store, tlsConfig, dflag, c.StringSlice("cluster-opt"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	var cl cluster.Cluster
-	switch c.String("cluster") {
+	switch c.String("cluster-driver") {
 	case "mesos":
-		cl = mesos.NewCluster(sched, store, options)
+		cl, err = mesos.NewCluster(sched, store, tlsConfig, dflag, c.StringSlice("cluster-opt"))
+		if err != nil {
+			log.Fatal(err)
+		}
 	case "swarm":
-		cl = swarm.NewCluster(sched, store, options)
+		cl, err = swarm.NewCluster(sched, store, tlsConfig, dflag, c.StringSlice("cluster-opt"))
+		if err != nil {
+			log.Fatal(err)
+		}
 	default:
 		log.Fatalf("Unsupported cluster %q", c.String("cluster"))
 	}

docker-archive/classicswarm

cli/manage.go
@@ -189,20 +189,17 @@ func manage(c *cli.Context) {
 	}
 
 	sched := scheduler.New(s, fs)
-
-	cluster, err := swarm.NewCluster(sched, store, tlsConfig, discovery, c.StringSlice("cluster-opt"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	var cl cluster.Cluster
-	switch c.String("cluster") {
+	switch c.String("cluster-driver") {
 	case "mesos":
-		cl = mesos.NewCluster(sched, store, options)
+		cl, err = mesos.NewCluster(sched, store, tlsConfig, discovery, c.StringSlice("cluster-opt"))
 	case "swarm":
-		cl = swarm.NewCluster(sched, store, options)
+		cl, err = swarm.NewCluster(sched, store, tlsConfig, discovery, c.StringSlice("cluster-opt"))
 	default:
 		log.Fatalf("Unsupported cluster %q", c.String("cluster"))
+	}
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	// see https://github.com/codegangsta/cli/issues/160