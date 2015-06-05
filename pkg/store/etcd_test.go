8ee1f84e27ca7f6397906438fd7db0f2432d78de
Alexandre Beslic <abronan@docker.com>
2015-06-05 11:48:46 -0700

    Add specific tests for each store
    Signed-off-by: Alexandre Beslic <abronan@docker.com>

--- pkg/store/etcd_test.go
+++ pkg/store/etcd_test.go
@@ -3,6 +3,8 @@ package store
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func makeEtcdClient(t *testing.T) Store {
@@ -35,4 +37,25 @@ func TestEtcdStore(t *testing.T) {
 	testPutEphemeral(t, kv)
 	testList(t, kv)
 	testDeleteTree(t, kv)
+}
+
+func TestCreateDirectory(t *testing.T) {
+	kv := makeEtcdClient(t)
+
+	etcd := kv.(*Etcd)
+
+	dir := "create/me/please"
+
+	// Create the directory
+	err := etcd.createDirectory(normalize(dir))
+	assert.NoError(t, err)
+
+	// The directory must exist
+	pair, err := kv.Get(dir)
+	assert.NoError(t, err)
+	if assert.NotNil(t, pair) {
+		assert.NotNil(t, pair.Value)
+	}
+	assert.Equal(t, pair.Value, []byte(""))
+	assert.NotEqual(t, pair.LastIndex, 0)
 }
