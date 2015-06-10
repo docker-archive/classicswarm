e6665cf91d4179afc3aa7b52d64bb420dd84fba0
Victor Vieux <victorvieux@gmail.com>
2015-06-09 17:30:14 -0700

    pretty print mesos offers
    Signed-off-by: Victor Vieux <victorvieux@gmail.com>

---
+++ cluster/mesos/utils.go
@@ -0,0 +1,39 @@
+package mesos
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/docker/docker/pkg/units"
+	"github.com/mesos/mesos-go/mesosproto"
+)
+
+func formatResource(resource *mesosproto.Resource) string {
+	switch resource.GetType() {
+	case mesosproto.Value_SCALAR:
+		if resource.GetName() == "disk" || resource.GetName() == "mem" {
+			return units.BytesSize(resource.GetScalar().GetValue() * 1024 * 1024)
+		}
+		return fmt.Sprintf("%d", int(resource.GetScalar().GetValue()))
+
+	case mesosproto.Value_RANGES:
+		var ranges []string
+		for _, r := range resource.GetRanges().GetRange() {
+			ranges = append(ranges, fmt.Sprintf("%d-%d", r.GetBegin(), r.GetEnd()))
+		}
+		return strings.Join(ranges, ", ")
+	}
+	return "?"
+}
+
+func sumScalarResourceValue(offers map[string]*mesosproto.Offer, name string) float64 {
+	var value float64
+	for _, offer := range offers {
+		for _, resource := range offer.Resources {
+			if *resource.Name == name {
+				value += *resource.Scalar.Value
+			}
+		}
+	}
+	return value
+}
