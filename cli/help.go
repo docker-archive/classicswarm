a41c5bf426382f6ec02e0c5971f021f99223b94b
Isabel Jimenez <contact.isabeljimenez@gmail.com>
2015-06-15 20:40:07 -0400

    making task timeout configurable
    Signed-off-by: Isabel Jimenez <contact.isabeljimenez@gmail.com>

--- cli/help.go
+++ cli/help.go
@@ -47,6 +47,7 @@ Options:
                                     {{printf "\t * mesos.address=\taddress to bind on [$SWARM_MESOS_ADDRESS]"}}
                                     {{printf "\t * mesos.port=\tport to bind on [$SWARM_MESOS_PORT]"}}
                                     {{printf "\t * mesos.offertimeout=10m\ttimeout for offers [$SWARM_MESOS_OFFER_TIMEOUT]"}}
+                                    {{printf "\t * mesos.tasktimeout=5s\ttimeout for task creation [$SWARM_MESOS_TASK_TIMEOUT]"}}
                                     {{printf "\t * mesos.user=\tframework user [$SWARM_MESOS_USER]"}}{{end}}{{ end }}
 `
 
