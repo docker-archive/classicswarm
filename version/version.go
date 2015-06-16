a439471fe62b15a966b7b2538b346d9077a124fe
Andrea Luzzardi <aluzzardi@gmail.com>
2015-06-16 15:10:27 -0700

    Bump version to 0.4.0
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// VERSION should be updated by hand at each release
-	VERSION = "0.3.0"
+	VERSION = "0.4.0"
 
 	// GITCOMMIT will be overwritten automatically by the build system
 	GITCOMMIT = "HEAD"
