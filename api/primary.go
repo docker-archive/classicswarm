5a217d6169f3e44a916012b7165b9a7c397a115d
Victor Vieux <vieux@docker.com>
2015-07-18 22:04:56 -0700

    add new endpoints for docker cp
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- api/primary.go
+++ api/primary.go
@@ -21,6 +21,9 @@ type context struct {
 type handler func(c *context, w http.ResponseWriter, r *http.Request)
 
 var routes = map[string]map[string]handler{
+	"HEAD": {
+		"/containers/{name:.*}/archive": proxyContainer,
+	},
 	"GET": {
 		"/_ping":                          ping,
 		"/events":                         getEvents,
@@ -35,6 +38,7 @@ var routes = map[string]map[string]handler{
 		"/images/{name:.*}/json":          proxyImage,
 		"/containers/ps":                  getContainersJSON,
 		"/containers/json":                getContainersJSON,
+		"/containers/{name:.*}/archive":   proxyContainer,
 		"/containers/{name:.*}/export":    proxyContainer,
 		"/containers/{name:.*}/changes":   proxyContainer,
 		"/containers/{name:.*}/json":      getContainerJSON,
@@ -67,6 +71,9 @@ var routes = map[string]map[string]handler{
 		"/containers/{name:.*}/exec":    postContainersExec,
 		"/exec/{execid:.*}/start":       proxyHijack,
 		"/exec/{execid:.*}/resize":      proxyContainer,
+	},
+	"PUT": {
+		"/containers/{name:.*}/archive": proxyContainer,
 	},
 	"DELETE": {
 		"/containers/{name:.*}": deleteContainers,
