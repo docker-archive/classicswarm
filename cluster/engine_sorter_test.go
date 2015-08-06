974a482d1624c1bcf75681569974b5db7884d2fe
Chanwit Kaewkasi <chanwit@gmail.com>
2015-08-06 10:59:34 -0400

    implemented naturalsorter for engine
    Signed-off-by: Chanwit Kaewkasi <chanwit@gmail.com>

--- cluster/engine_sorter_test.go
+++ cluster/engine_sorter_test.go
@@ -16,3 +16,17 @@ func TestEngineSorter(t *testing.T) {
 	assert.Equal(t, engines[1].Name, "name2")
 	assert.Equal(t, engines[2].Name, "name3")
 }
+
+func TestNaturalSort(t *testing.T) {
+	engines := []*Engine{{Name: "machine10"},
+		{Name: "machine9"},
+		{Name: "machine1"},
+		{Name: "machine11"}}
+
+	sort.Sort(EngineSorter(engines))
+
+	assert.Equal(t, engines[0].Name, "machine1")
+	assert.Equal(t, engines[1].Name, "machine9")
+	assert.Equal(t, engines[2].Name, "machine10")
+	assert.Equal(t, engines[3].Name, "machine11")
+}
