82b3c2f344c369c6f1d59e4f0c992883778c2d32
Victor Vieux <vieux@docker.com>
2015-08-15 14:22:13 -0700

    choose docker-compose version
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- test/integration/test_runner.sh
+++ test/integration/test_runner.sh
@@ -18,7 +18,7 @@ TESTS=${@:-. compose discovery api mesos/api mesos/compose}
 export SWARM_BINARY=`mktemp`
 
 # Download docker-compose
-execute time curl -L --silent https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+execute time curl -L --silent https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 execute chmod +x /usr/local/bin/docker-compose
 
 # Build Swarm.
