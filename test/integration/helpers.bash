829db076c313d9b9487505d2a2093d9f36eac42e
Victor Vieux <vieux@docker.com>
2015-09-23 12:06:32 -0700

    update compose in tests
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- test/integration/helpers.bash
+++ test/integration/helpers.bash
@@ -16,7 +16,7 @@ SWARM_BINARY=${SWARM_BINARY:-${SWARM_ROOT}/swarm}
 DOCKER_IMAGE=${DOCKER_IMAGE:-dockerswarm/dind-master}
 DOCKER_VERSION=${DOCKER_VERSION:-latest}
 DOCKER_BINARY=${DOCKER_BINARY:-`command -v docker`}
-DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-1.4.0}
+DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-1.4.2}
 
 # Port on which the manager will listen to (random port between 6000 and 7000).
 SWARM_BASE_PORT=$(( ( RANDOM % 1000 )  + 6000 ))
