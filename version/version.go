2b62d103be26bdbfc979d3dce3987111bf33b5f6
Victor Vieux <vieux@docker.com>
2015-10-13 17:40:00 -0700

    Bump version to 1.0.0
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// VERSION should be updated by hand at each release
-	VERSION = "0.5.0-dev"
+	VERSION = "1.0.0"
 
 	// GITCOMMIT will be overwritten automatically by the build system
 	GITCOMMIT = "HEAD"
