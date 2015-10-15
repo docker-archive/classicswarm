9e8f540697b0e0a7aa8fd9a1d8f1c767249e7529
Daniel Nephin <dnephin@docker.com>
2015-10-15 12:21:54 -0400

    Use ParseRepositoryTags from engine.
    Signed-off-by: Daniel Nephin <dnephin@docker.com>

--- cluster/image.go
+++ cluster/image.go
@@ -3,6 +3,7 @@ package cluster
 import (
 	"strings"
 
+	"github.com/docker/docker/pkg/parsers"
 	"github.com/samalba/dockerclient"
 )
 
@@ -13,59 +14,24 @@ type Image struct {
 	Engine *Engine
 }
 
-func toImageName(repo string, name string, tag string) string {
-	fullname := name
-	if tag != "" {
-		fullname = name + ":" + tag
-	}
-	if repo != "" {
-		fullname = repo + "/" + fullname
-	}
-	return fullname
-}
-
-func parseImageName(fullname string) (repo string, name string, tag string) {
-	parts := strings.SplitN(fullname, "/", 2)
-
-	nameAndTag := parts[0]
-	if len(parts) == 2 {
-		repo = parts[0]
-		nameAndTag = parts[1]
-	}
-
-	parts = strings.SplitN(nameAndTag, ":", 2)
-	name = parts[0]
-	if len(parts) == 2 {
-		tag = parts[1]
-	}
-
-	return
-}
-
 // Match is exported
 func (image *Image) Match(IDOrName string, matchTag bool) bool {
 	size := len(IDOrName)
 
+	// TODO: prefix match can cause false positives with image names
 	if image.Id == IDOrName || (size > 2 && strings.HasPrefix(image.Id, IDOrName)) {
 		return true
 	}
 
-	imageName := IDOrName
-	repo, name, tag := parseImageName(imageName)
-	if matchTag {
-		if tag == "" {
-			imageName = toImageName(repo, name, "latest")
-		}
-	} else {
-		imageName = toImageName(repo, name, "")
-	}
+	repoName, tag := parsers.ParseRepositoryTag(IDOrName)
 
-	for _, repoTag := range image.RepoTags {
-		if matchTag == false {
-			r, n, _ := parseImageName(repoTag)
-			repoTag = toImageName(r, n, "")
+	for _, imageRepoTag := range image.RepoTags {
+		imageRepoName, imageTag := parsers.ParseRepositoryTag(imageRepoTag)
+
+		if matchTag == false && imageRepoName == repoName {
+			return true
 		}
-		if repoTag == imageName {
+		if imageRepoName == repoName && (imageTag == tag || tag == "") {
 			return true
 		}
 	}
