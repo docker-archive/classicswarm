8932b69e79bd933329a5202871d63d3b581ec546
Kelsey Hightower <kelsey.hightower@gmail.com>
2015-10-30 20:57:40 -0700

    update kubernetes integration test helper
    Signed-off-by: Kelsey Hightower <kelsey.hightower@gmail.com>
    Signed-off-by: Patrick Reilly <patrick@kismatic.com>

--- test/integration/kubernetes/kubernetes_helpers.bash
+++ test/integration/kubernetes/kubernetes_helpers.bash
@@ -2,90 +2,51 @@
 
 load ../../helpers
 
-KUBERNETES_IMAGE="b.kuar.io/kuar/kubernetes:1.2.0"
-ETCD_IMAGE="b.gcr.io/kuar/etcd:2.2.0"
+KUBERNETES_IMAGE="gcr.io/google_containers/hyperkube:v1.0.6"
+ETCD_IMAGE="gcr.io/google_containers/etcd:2.0.12"
 
 # Start kubernetes controller components and kubelet.
 function start_kubernetes() {
     ETCD=$(
       docker_host run -d \
-        --name etcd \
+        --name=etcd \
         --net=host \
-        $ETCD_IMAGE \
-        --advertise-client-urls http://127.0.0.1:2379
-        --data-dir /var/lib/etcd
-        --listen-client-urls http://127.0.0.1:2379
-        --listen-peer-urls http://127.0.0.1:2380
-        --name etcd0
+        $ETCD_IMAGE /usr/local/bin/etcd \
+        --addr=127.0.0.1:4001 \
+        --bind-addr=0.0.0.0:4001 \
+        --data-dir=/var/etcd/data
     )
 
     retry 10 1 eval "docker_host ps | grep 'etcd'"
 
-    KUBE_APISERVER=$(
-      docker_host run -d \
-        --name kube-apiserver \
+    KUBERNETES=$(
+      docker run -d \
+        --name=kubernetes \
+        --volume=/:/rootfs:ro \
+        --volume=/sys:/sys:ro \
+        --volume=/dev:/dev \
+        --volume=/var/lib/docker/:/var/lib/docker:rw \
+        --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
+        --volume=/var/run:/var/run:rw \
         --net=host \
-        $KUBERNETES_IMAGE kube-apiserver \
-        --etcd-servers=http://127.0.0.1:2379 \
-        --insecure-bind-address=127.0.0.1 \
-        --insecure-port=8080 \
-        --logtostderr=true \
-        --service-cluster-ip-range=10.200.20.0/24 \
-        --v=2
-    )
-
-    retry 10 1 eval "docker_host ps | grep 'kube-apiserver'"
-
-    KUBE_CONTROLLER_MANAGER=$(
-      docker_host run -d \
-        --name kube-controller-manager \
-        --net=host \
-        $KUBERNETES_IMAGE kube-controller-manager \
-        --logtostderr=true \
-        --master=http://127.0.0.1:8080 \
-        --v=2
-    )
-
-    retry 10 1 eval "docker_host ps | grep 'kube-controller-manager'"
-
-    KUBE_SCHEDULER=$(
-      docker_host run -d \
-        --name=kube-scheduler \
-        --net=host \
-        $KUBERNETES_IMAGE kube-scheduler \
-        --logtostderr=true \
-        --master=http://127.0.0.1:8080 \
-        --v=2
-    )
-
-    retry 10 1 eval "docker_host ps | grep 'kube-scheduler'"
-
-    KUBELET=$(
-      docker_host run -d \
-        --name=kubelet \
-        --net=host \
-        $KUBERNETES_IMAGE kubelet \
-        --address=0.0.0.0 \
-        --api-servers=http://127.0.0.1:8080 \
+        --pid=host \
+        --privileged=true \
+        $KUBERNETES_IMAGE \
+        /hyperkube kubelet \
         --containerized \
-        --enable-server \
-        --hostname-override=127.0.0.1 \
-        --v=2
+        --hostname-override="127.0.0.1" \
+        --address="0.0.0.0" \
+        --api-servers=http://127.0.0.1:8080 \
+        --config=/etc/kubernetes/manifests
     )
 
-    retry 10 1 eval "docker_host ps | grep 'kubelet'"
+    retry 10 1 eval "docker_host ps | grep 'kubernetes'"
 }
 
 # Stop Kubernetes controller components and Kubelet.
 function stop_kubernetes() {
-    echo "Stopping $KUBE_CONTROLLER_MANAGER"
-    docker_host rm -f -v $KUBE_CONTROLLER_MANAGER > /dev/null;
-    echo "Stopping $KUBE_SCHEDULER"
-    docker_host rm -f -v $KUBE_SCHEDULER > /dev/null;
-    echo "Stopping $KUBELET"
-    docker_host rm -f -v $KUBELET > /dev/null;
-    echo "Stopping $KUBE_APISERVER"
-    docker_host rm -f -v $KUBE_APISERVER > /dev/null;
+    echo "Stopping $KUBERNETES"
+    docker_host rm -f -v $KUBERNETES > /dev/null;
     echo "Stopping $ETCD"
     docker_host rm -f -v $ETCD > /dev/null;
 }
