d51ee2f161a0ea278c2126324ce4aab28f52303d
bingshen.wbs <bingshen.wbs@alibaba-inc.com>
2015-11-28 19:36:03 +0800

    fix list networks with filter {name: <overlay_network_name>} fix fail to inspect node/<overlay_network_name> bug
    Signed-off-by: bingshen.wbs <bingshen.wbs@alibaba-inc.com>

--- api/handlers.go
+++ api/handlers.go
@@ -720,7 +720,7 @@ func deleteNetworks(c *context, w http.ResponseWriter, r *http.Request) {
 
 	var id = mux.Vars(r)["networkid"]
 
-	if network := c.cluster.Networks().Uniq().Get(id); network != nil {
+	if network := c.cluster.Networks().Get(id); network != nil {
 		if err := c.cluster.RemoveNetwork(network); err != nil {
 			httpError(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -761,7 +761,7 @@ func ping(c *context, w http.ResponseWriter, r *http.Request) {
 // Proxy a request to the right node
 func proxyNetwork(c *context, w http.ResponseWriter, r *http.Request) {
 	var id = mux.Vars(r)["networkid"]
-	if network := c.cluster.Networks().Uniq().Get(id); network != nil {
+	if network := c.cluster.Networks().Get(id); network != nil {
 
 		// Set the network ID in the proxied URL path.
 		r.URL.Path = strings.Replace(r.URL.Path, id, network.ID, 1)
