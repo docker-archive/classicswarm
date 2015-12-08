c897e30d7dd46d4554f8e71f339bad4458119270
Victor Vieux <vieux@docker.com>
2015-12-08 03:33:17 -0800

    update docker godeps
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- Godeps/_workspace/src/github.com/docker/docker/pkg/units/duration.go
+++ Godeps/_workspace/src/github.com/docker/docker/pkg/units/duration.go
@@ -1,3 +1,5 @@
+// Package units provides helper function to parse and print size and time units
+// in human-readable format.
 package units
 
 import (
@@ -6,7 +8,7 @@ import (
 )
 
 // HumanDuration returns a human-readable approximation of a duration
-// (eg. "About a minute", "4 hours ago", etc.)
+// (eg. "About a minute", "4 hours ago", etc.).
 func HumanDuration(d time.Duration) string {
 	if seconds := int(d.Seconds()); seconds < 1 {
 		return "Less than a second"
@@ -26,7 +28,6 @@ func HumanDuration(d time.Duration) string {
 		return fmt.Sprintf("%d weeks", hours/24/7)
 	} else if hours < 24*365*2 {
 		return fmt.Sprintf("%d months", hours/24/30)
-	} else {
-		return fmt.Sprintf("%d years", hours/24/365)
 	}
+	return fmt.Sprintf("%d years", int(d.Hours())/24/365)
 }
