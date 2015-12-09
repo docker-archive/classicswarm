3c3b56c42c8f4525d185e12a875e869c9c6ec272
Dong Chen <dongluo.chen@docker.com>
2015-12-09 15:39:20 -0800

    Bump version to 1.0.1
    Signed-off-by: Dong Chen <dongluo.chen@docker.com>
    Signed-off-by: Klaus Ma <klaus1982.cn@gmail.com>

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// VERSION should be updated by hand at each release
-	VERSION = "1.0.0"
+	VERSION = "1.0.1"
 
 	// GITCOMMIT will be overwritten automatically by the build system
 	GITCOMMIT = "HEAD"
