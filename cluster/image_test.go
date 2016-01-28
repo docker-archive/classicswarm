fff2db424c883598ddacdc06a645474ee142f2be
Victor Vieux <vieux@docker.com>
2016-01-28 13:07:25 -0800

    fix unit test and add integration test
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- cluster/image_test.go
+++ cluster/image_test.go
@@ -126,19 +126,19 @@ func TestImagesFilterWithNameFilterWithTag(t *testing.T) {
 
 func TestParseRepositoryTag(t *testing.T) {
 
-	repo, tag := parseRepositoryTag("localhost.localdomain:5000/samalba/hipache:latest")
+	repo, tag := ParseRepositoryTag("localhost.localdomain:5000/samalba/hipache:latest")
 	if tag != "latest" {
 		t.Errorf("repo=%s tag=%s", repo, tag)
 	}
-	repo, tag = parseRepositoryTag("localhost:5000/foo/bar@sha256:bc8813ea7b3603864987522f02a76101c17ad122e1c46d790efc0fca78ca7bfb")
+	repo, tag = ParseRepositoryTag("localhost:5000/foo/bar@sha256:bc8813ea7b3603864987522f02a76101c17ad122e1c46d790efc0fca78ca7bfb")
 	if tag != "sha256:bc8813ea7b3603864987522f02a76101c17ad122e1c46d790efc0fca78ca7bfb" {
 		t.Logf("repo=%s tag=%s", repo, tag)
 	}
-	repo, tag = parseRepositoryTag("localhost:5000/foo/bar")
+	repo, tag = ParseRepositoryTag("localhost:5000/foo/bar")
 	if tag != "" {
 		t.Logf("repo=%s tag=%s", repo, tag)
 	}
-	repo, tag = parseRepositoryTag("localhost:5000/foo/bar:latest")
+	repo, tag = ParseRepositoryTag("localhost:5000/foo/bar:latest")
 	t.Logf("repo=%s tag=%s", repo, tag)
 	if tag != "latest" {
 		t.Logf("repo=%s tag=%s", repo, tag)
