72ec783e07b02333d148b886ea6220a1f8fb0ea2
andreyod <root@vagrant.vm>
2016-03-08 08:04:55 +0000

    fix containers other

--- pkg/authZ/defaultAuthZImpl.go
+++ pkg/authZ/defaultAuthZImpl.go
@@ -270,7 +270,7 @@ func (*DefaultImp) HandleEvent(eventType states.EventEnum, w http.ResponseWriter
 				freeResources(quota, nil, r.Header.Get(headers.AuthZTenantIdHeaderName), 0, dto.ContainerID, rec.Code)
 			}
 			// copy everything from recorder to writer
-              	w.WriteHeader(rec.Code)
+              		w.WriteHeader(rec.Code)
         		for k, v := range rec.HeaderMap {
             			w.Header()[k] = v
         		}
