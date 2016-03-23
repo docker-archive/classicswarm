742295d74e9bb5f01f3ba0c6297fedb69ce13d6c
Nishant Totla <nishanttotla@gmail.com>
2016-03-23 11:32:46 -0700

    Updating cluster unit tests
    Signed-off-by: Nishant Totla <nishanttotla@gmail.com>

--- cluster/container_test.go
+++ cluster/container_test.go
@@ -3,33 +3,35 @@ package cluster
 import (
 	"testing"
 
-	"github.com/samalba/dockerclient"
+	"github.com/docker/engine-api/types"
+	containertypes "github.com/docker/engine-api/types/container"
+	networktypes "github.com/docker/engine-api/types/network"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestContainersGet(t *testing.T) {
 	containers := Containers([]*Container{{
-		Container: dockerclient.Container{
-			Id:    "container1-id",
+		Container: types.Container{
+			ID:    "container1-id",
 			Names: []string{"/container1-name1", "/container1-name2"},
 		},
 		Engine: &Engine{ID: "test-engine"},
-		Config: BuildContainerConfig(dockerclient.ContainerConfig{
+		Config: BuildContainerConfig(containertypes.Config{
 			Labels: map[string]string{
 				"com.docker.swarm.id": "swarm1-id",
 			},
-		}),
+		}, containertypes.HostConfig{}, networktypes.NetworkingConfig{}),
 	}, {
-		Container: dockerclient.Container{
-			Id:    "container2-id",
+		Container: types.Container{
+			ID:    "container2-id",
 			Names: []string{"/con"},
 		},
 		Engine: &Engine{ID: "test-engine"},
-		Config: BuildContainerConfig(dockerclient.ContainerConfig{
+		Config: BuildContainerConfig(containertypes.Config{
 			Labels: map[string]string{
 				"com.docker.swarm.id": "swarm2-id",
 			},
-		}),
+		}, containertypes.HostConfig{}, networktypes.NetworkingConfig{}),
 	}})
 
 	// Invalid lookup
@@ -54,5 +56,5 @@ func TestContainersGet(t *testing.T) {
 	// Get name before ID prefix
 	cc := containers.Get("con")
 	assert.NotNil(t, cc)
-	assert.Equal(t, cc.Id, "container2-id")
+	assert.Equal(t, cc.ID, "container2-id")
 }
