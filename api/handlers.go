1572126b7f38f0a6ae478e65afa7b625b8132654
Victor Vieux <vieux@docker.com>
2016-03-25 15:40:37 -0700

    more network
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- api/handlers.go
+++ api/handlers.go
@@ -951,7 +951,7 @@ func proxyNetworkDisconnect(c *context, w http.ResponseWriter, r *http.Request) 
 	r.Body = ioutil.NopCloser(bytes.NewBuffer(buf))
 
 	// Extract container info from r.Body copy
-	var disconnect dockerclient.NetworkDisconnect
+	var disconnect apitypes.NetworkDisconnect
 	if err := json.NewDecoder(bodyCopy).Decode(&disconnect); err != nil {
 		httpError(w, fmt.Sprintf("Container is not specified"), http.StatusNotFound)
 		return
@@ -1007,7 +1007,7 @@ func proxyNetworkConnect(c *context, w http.ResponseWriter, r *http.Request) {
 	r.Body = ioutil.NopCloser(bytes.NewBuffer(buf))
 
 	// Extract container info from r.Body copy
-	var connect dockerclient.NetworkConnect
+	var connect apitypes.NetworkConnect
 	if err := json.NewDecoder(bodyCopy).Decode(&connect); err != nil {
 		httpError(w, fmt.Sprintf("Container is not specified"), http.StatusNotFound)
 		return
