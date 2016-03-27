f0db55d999ea90a36f4509d83a211ccdf3cfba32
Sun Hongliang <allen.sun@daocloud.io>
2016-03-27 18:36:16 +0800

    make 2xx response status code compatible with docker 1. POST /images/load    from 201 to 200 2. POST /images/{name:.*}/tag from 200 to 201 3. POST /containers/{name:.*}/rename from 200 to 204
    Signed-off-by: Sun Hongliang <allen.sun@daocloud.io>

--- api/handlers.go
+++ api/handlers.go
@@ -704,7 +704,7 @@ func postImagesCreate(c *context, w http.ResponseWriter, r *http.Request) {
 // POST /images/load
 func postImagesLoad(c *context, w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusOK)
 
 	// call cluster to load image on every node
 	wf := NewWriteFlusher(w)
@@ -1154,6 +1154,7 @@ func postTagImage(c *context, w http.ResponseWriter, r *http.Request) {
 			httpError(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
+	w.WriteHeader(http.StatusCreated)
 }
 
 // Proxy a request to a random node
@@ -1299,6 +1300,7 @@ func postRenameContainer(c *context, w http.ResponseWriter, r *http.Request) {
 			httpError(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
+	w.WriteHeader(http.StatusNoContent)
 
 }
 
