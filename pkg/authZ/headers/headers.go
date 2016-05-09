ff415df8db334f9abd73d95d0d6075631d8b83ae
andreyod <root@vagrant.vm>
2016-05-09 01:27:20 +0000

    remove old packages.

--- pkg/authZ/headers/headers.go
+++
@@ -1,7 +0,0 @@
-package headers
-
-var AuthZTokenHeaderName = "X-Auth-Token"
-
-var TenancyLabel = "com.swarm.tenant.0"
-
-var AuthZTenantIdHeaderName = "X-Auth-TenantId"
--- pkg/multiTenancy/handler/handler.go
+++
@@ -1,8 +0,0 @@
-package handler
-
-import (
-	"net/http"
-	"github.com/docker/swarm/cluster"
-)
-
-type Handler func(command string, cluster cluster.Cluster, w http.ResponseWriter, r *http.Request, swarmHandler http.Handler) error
