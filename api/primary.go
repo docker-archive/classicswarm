afff541f41c4dd581d396ff11fd3bfe130c81122
Sun Hongliang <allen.sun@daocloud.io>
2016-05-13 15:02:57 +0800

    add expvar into Swarm to show public variables and fix a pprof URL
    Signed-off-by: Sun Hongliang <allen.sun@daocloud.io>

--- api/primary.go
+++ api/primary.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"crypto/tls"
+	"expvar"
+	"fmt"
 	"net/http"
 
 	"net/http/pprof"
@@ -103,15 +105,31 @@ func writeCorsHeaders(w http.ResponseWriter, r *http.Request) {
 
 func profilerSetup(mainRouter *mux.Router, path string) {
 	var r = mainRouter.PathPrefix(path).Subrouter()
+	r.HandleFunc("/vars", expVars)
 	r.HandleFunc("/pprof/", pprof.Index)
 	r.HandleFunc("/pprof/cmdline", pprof.Cmdline)
 	r.HandleFunc("/pprof/profile", pprof.Profile)
 	r.HandleFunc("/pprof/symbol", pprof.Symbol)
-	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	r.HandleFunc("/pprof/trace", pprof.Trace)
 	r.HandleFunc("/pprof/block", pprof.Handler("block").ServeHTTP)
 	r.HandleFunc("/pprof/heap", pprof.Handler("heap").ServeHTTP)
 	r.HandleFunc("/pprof/goroutine", pprof.Handler("goroutine").ServeHTTP)
 	r.HandleFunc("/pprof/threadcreate", pprof.Handler("threadcreate").ServeHTTP)
+}
+
+// Replicated from expvar.go as not public.
+func expVars(w http.ResponseWriter, r *http.Request) {
+	first := true
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	fmt.Fprintf(w, "{\n")
+	expvar.Do(func(kv expvar.KeyValue) {
+		if !first {
+			fmt.Fprintf(w, ",\n")
+		}
+		first = false
+		fmt.Fprintf(w, "%q: %s", kv.Key, kv.Value)
+	})
+	fmt.Fprintf(w, "\n}\n")
 }
 
 // NewPrimary creates a new API router.
