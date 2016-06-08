5a98af5ba0f93775a0eaec185029167b1517d83c
Alexander Brand <abrand@apprenda.com>
2016-06-08 09:22:33 -0400

    Fixing tests and gofmt issue
    Signed-off-by: Alexander Brand <abrand@apprenda.com>

--- cluster/kubernetes/cluster.go
+++ cluster/kubernetes/cluster.go
@@ -1,4 +1,4 @@
-﻿package kubernetes
+package kubernetes
 
 import (
 	"crypto/tls"
--- cluster/kubernetes/cluster_test.go
+++ cluster/kubernetes/cluster_test.go
@@ -3,6 +3,7 @@ package kubernetes
 import (
 	"testing"
 
+	"github.com/docker/swarm/cluster"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/resource"
@@ -11,7 +12,7 @@ import (
 
 func TestNewCluster(t *testing.T) {
 	client := testclient.NewSimpleFake()
-	_, err := NewCluster(nil, "", client, nil)
+	_, err := NewCluster(nil, "", client, nil, nil)
 	assert.Nil(t, err)
 }
 
@@ -25,7 +26,7 @@ func TestInfo(t *testing.T)            {}
 
 func TestTotalCpus(t *testing.T) {
 	client := testclient.NewSimpleFake()
-	c, err := NewCluster(nil, "", client, nil)
+	c, err := NewCluster(nil, "", client, nil, nil)
 	assert.Nil(t, err)
 	assert.Equal(t, c.TotalCpus(), int64(0))
 
@@ -41,14 +42,14 @@ func TestTotalCpus(t *testing.T) {
 			},
 		},
 	}})
-	c, err = NewCluster(nil, "", client, nil)
+	c, err = NewCluster(nil, "", client, nil, &cluster.EngineOpts{})
 	assert.Nil(t, err)
 	assert.Equal(t, c.TotalCpus(), int64(2))
 }
 
 func TestTotalMemory(t *testing.T) {
 	client := testclient.NewSimpleFake()
-	c, err := NewCluster(nil, "", client, nil)
+	c, err := NewCluster(nil, "", client, nil, &cluster.EngineOpts{})
 	assert.Nil(t, err)
 	assert.Equal(t, c.TotalMemory(), int64(0))
 
@@ -64,7 +65,7 @@ func TestTotalMemory(t *testing.T) {
 			},
 		},
 	}})
-	c, err = NewCluster(nil, "", client, nil)
+	c, err = NewCluster(nil, "", client, nil, &cluster.EngineOpts{})
 	assert.Nil(t, err)
 	assert.Equal(t, c.TotalMemory(), int64(1024))
 }
@@ -87,21 +88,21 @@ func TestFormatContainerID(t *testing.T) {
 func TestDockerEngines(t *testing.T) {
 	// Test empty nodes.
 	client := testclient.NewSimpleFake()
-	engines, err := dockerEngines(client)
+	engines, err := dockerEngines(client, &cluster.EngineOpts{})
 	assert.Nil(t, err)
 	assert.Len(t, engines, 0, "The size of engines is not 0")
 
 	// Test non-empty nodes.
 	client = testclient.NewSimpleFake(&api.NodeList{Items: []api.Node{
 		{ObjectMeta: api.ObjectMeta{Name: "node1"}}}})
-	engines, err = dockerEngines(client)
+	engines, err = dockerEngines(client, &cluster.EngineOpts{})
 	assert.Nil(t, err)
 	assert.Len(t, engines, 1, "The size of engines is not 1")
 }
 
 func TestRemoveImages(t *testing.T) {
 	client := testclient.NewSimpleFake()
-	c, err := NewCluster(nil, "", client, nil)
+	c, err := NewCluster(nil, "", client, nil, &cluster.EngineOpts{})
 	assert.Nil(t, err)
 
 	_, err = c.RemoveImages("image-1", false)
@@ -110,16 +111,16 @@ func TestRemoveImages(t *testing.T) {
 
 func TestCreateNetwork(t *testing.T) {
 	client := testclient.NewSimpleFake()
-	c, err := NewCluster(nil, "", client, nil)
+	c, err := NewCluster(nil, "", client, nil, &cluster.EngineOpts{})
 	assert.Nil(t, err)
 
-	_, err = c.CreateNetwork(nil)
+	_, err = c.CreateNetwork("", nil)
 	assert.EqualError(t, err, "not supported with kubernetes")
 }
 
 func TestRemoveNetwork(t *testing.T) {
 	client := testclient.NewSimpleFake()
-	c, err := NewCluster(nil, "", client, nil)
+	c, err := NewCluster(nil, "", client, nil, &cluster.EngineOpts{})
 	assert.Nil(t, err)
 
 	assert.EqualError(t, c.RemoveNetwork(nil), "not supported with kubernetes")
@@ -127,7 +128,7 @@ func TestRemoveNetwork(t *testing.T) {
 
 func TestRenameContainer(t *testing.T) {
 	client := testclient.NewSimpleFake()
-	c, err := NewCluster(nil, "", client, nil)
+	c, err := NewCluster(nil, "", client, nil, &cluster.EngineOpts{})
 	assert.Nil(t, err)
 
 	assert.EqualError(t, c.RenameContainer(nil, ""), "not supported with kubernetes")
