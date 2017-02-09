b31bbf916e91345c356b67ad24f829ac6cfeca2b
fugr <fugr2012@gmail.com>
2017-02-09 15:56:00 +0800

    fix warnings found by go vet  1.can't check non-constant format in call to Errorf  2.can't check non-constant format in call to Fatalf  3.can't check non-constant format in call to Fprintf  4.composite literal uses unkeyed fields
    Signed-off-by: fugr <fugr2012@gmail.com>

--- cli/cli.go
+++ cli/cli.go
@@ -48,7 +48,7 @@ func Run() {
 		log.SetOutput(os.Stderr)
 		level, err := log.ParseLevel(c.String("log-level"))
 		if err != nil {
-			log.Fatalf(err.Error())
+			log.Fatal(err.Error())
 		}
 		log.SetLevel(level)
 
