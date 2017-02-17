d4d068454dabfc9785ba215bf4afb2b0e2eb7b3d
Mike Simons <mike.simons@hpe.com>
2017-02-17 11:48:10 +0000

    Completed simplified schema
    Signed-off-by: Mike Simons <mike.simons@hpe.com>

--- scheduler/simplified-schema/schema.go
+++ scheduler/simplified-schema/schema.go
@@ -1,35 +1,37 @@
 package simplifiedschema
 
 import (
+	apitypes "github.com/docker/engine-api/types"
 	"github.com/docker/swarm/cluster"
 	"github.com/docker/swarm/scheduler/node"
 )
 
-type Config struct {
-	Image     string
-	CpuShares int
-	MemShares int
-}
-
+// Container represents a container in simplified form
 type Container struct {
-	Names  []string          `json:"names"`
-	Image  string            `json:"image"`
-	State  string            `json:"state"`
-	Ports  []int             `json:"ports"`
-	Labels map[string]string `json:"labels"`
+	Id      string            `json:"id"`
+	Names   []string          `json:"names"`
+	Image   string            `json:"image"`
+	ImageId string            `json:"image_id"`
+	State   string            `json:"state"`
+	Ports   []apitypes.Port   `json:"ports"`
+	Labels  map[string]string `json:"labels"`
 }
 
+// Image represents a image in simplified form
 type Image struct {
-	RepoTags    []string          `json:"repo_tags"`
-	RepoDigests []string          `json:"repo_digests"`
-	Labels      map[string]string `json:"labels"`
+	Id      string            `json:"id"`
+	Tags    []string          `json:"repo_tags"`
+	Digests []string          `json:"repo_digests"`
+	Labels  map[string]string `json:"labels"`
 }
 
+// Resource represents some kind of consumable resource (e.g. cpu / ram)
 type Resource struct {
 	Used  int64 `json:"used"`
 	Total int64 `json:"total"`
 }
 
+// Node represents a node in simplified form
 type Node struct {
 	ID              string            `json:"id"`
 	HealthIndicator int64             `json:"scheduling_health"`
@@ -40,32 +42,36 @@ type Node struct {
 	Images          []Image           `json:"images"`
 	CPUs            Resource          `json:"cpus"`
 	Memory          Resource          `json:"memory"`
-	//EngineVersion   string            `json:"engine_version"`
+	EngineVersion   string            `json:"engine_version"`
 }
 
+// Placement is the top level simplified placement structure
 type Placement struct {
 	Config cluster.ContainerConfig `json:"config"`
 	Nodes  []Node                  `json:"nodes"`
 }
 
+// SimplifyPlacement takes a placement request and a list of nodes and simplfies it for wire transfer
 func SimplifyPlacement(config *cluster.ContainerConfig, nodes []*node.Node) *Placement {
 	placement := &Placement{}
 	placement.Config = *config
 
 	for _, node := range nodes {
-		placement.Nodes = append(placement.Nodes, SimplifyNode(node))
+		placement.Nodes = append(placement.Nodes, simplifyNode(node))
 	}
 
 	return placement
 }
 
-func SimplifyNode(node *node.Node) Node {
+// simplifyNode simplifies an individual node record
+func simplifyNode(node *node.Node) Node {
 	var images []Image
 	for _, image := range node.Images {
 		simple_image := Image{
-			RepoTags:    image.RepoTags,
-			RepoDigests: image.RepoDigests,
-			Labels:      image.Labels,
+			Id:      image.ID,
+			Tags:    image.RepoTags,
+			Digests: image.RepoDigests,
+			Labels:  image.Labels,
 		}
 		images = append(images, simple_image)
 	}
@@ -73,11 +79,13 @@ func SimplifyNode(node *node.Node) Node {
 	var containers []Container
 	for _, container := range node.Containers {
 		simple_container := Container{
-			Names: container.Names,
-			Image: container.Image,
-			State: container.State,
-			//Ports:  container.Ports,
-			Labels: container.Labels,
+			Id:      container.ID,
+			Names:   container.Names,
+			Image:   container.Image,
+			ImageId: container.ImageID,
+			State:   container.State,
+			Ports:   container.Ports,
+			Labels:  container.Labels,
 		}
 		containers = append(containers, simple_container)
 	}
@@ -87,6 +95,10 @@ func SimplifyNode(node *node.Node) Node {
 		HealthIndicator: node.HealthIndicator,
 		Name:            node.Name,
 		Addr:            node.Addr,
+		Labels:          node.Labels,
+		Containers:      containers,
+		Images:          images,
+		EngineVersion:   node.Engine.Version,
 		CPUs: Resource{
 			Total: node.TotalCpus,
 			Used:  node.UsedCpus,
@@ -95,8 +107,5 @@ func SimplifyNode(node *node.Node) Node {
 			Total: node.TotalMemory,
 			Used:  node.UsedMemory,
 		},
-		Labels:     node.Labels,
-		Containers: containers,
-		Images:     images,
 	}
 }
